2e7b43a2f71840205176b050d3068ef38beb711f
Harsh-Nikam07 <134618980+Harsh-Nikam07@users.noreply.github.com>
2024-11-19 21:49:16 +0530

    create trip from data validations and ui completion

--- src/constants/options.jsx
+++ src/constants/options.jsx
@@ -4,28 +4,36 @@ export const SelectTravelesList = [
         title: 'Just me',
         desc: "A sole traveler looking for a relaxing getaway",
         icon:'✈️',
-        people:1
+        people:1,
+        toolTipText:"The model is assuming 1 person if you select this,",
+        estimatedBudget: 'Assumed budget $500 - $1,000',
     },
     {
         id:2,
         title:'Couple',
         desc:" Two people looking for a romantic getaway",
-        icon:'💑',
-        people:2
+        icon:'🥂',
+        people:2,
+        toolTipText:"The model is assuming 2 person if you select this,",
+        estimatedBudget: 'Assumed budget $1,500 - $2,5000',
     },
     {
         id:3,
         title:'Family',
         desc:"A family of 4 looking for a fun-filled getaway",
-        icon:'👨‍👩‍👧‍👦',
-        people:4
+        icon:'🏡',
+        people: {min:3, max:8},
+        toolTipText:"The model is assuming 3-8 person if you select this,",
+        estimatedBudget: 'Assumed budget $2,500 - $6,000',
     },
     {
         id:4,
         title:'Friends',
         desc:"A group of friends looking for a lively adventure",
-        icon:'👫',
-        people:5
+        icon:'⛵',
+        people: {min:4, max:10},
+        toolTipText:"The model is assuming 4-10 person if you select this,",
+        estimatedBudget: 'Assumed budget $5,500 - $10,000',
     }
 ]
 
@@ -34,19 +42,22 @@ export const SelectBudgetOptions = [
         id:1,
         title:'Cheap',
         desc:"Stays conscious of costs",
-        icon:'💵'
+        icon:'💵',
+        estimatedBudget: 'The model is assuming budget of $500 - $1,000 (may vary)',
     },
     {
         id:2,
         title:'Moderate',
         desc:"Keeps costs on average side",
-        icon:'💰'
+        icon:'💰',
+        estimatedBudget: 'The model is assuming budget of $2,500 - $4,000 (may vary)',
     },
     {
         id:3,
         title:'Luxury',
         desc:"Don`t care about the costs",
-        icon:'💸'
+        icon:'💸',
+        estimatedBudget: 'The model is assuming budget of $4,000+ (may vary)',
     }
 ]
 
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,13 +1,47 @@
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Input } from '@/components/ui/input';
+import { SelectBudgetOptions, SelectTravelesList } from '@/constants/options';
+import { Button } from '@/components/ui/button';
+import { FaQuestion } from "react-icons/fa6";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 
 const CreateTrip = () => {
-  const [place, setPlace] = useState(null);
+  const [place, setPlace] = useState();
+
+  const [formData, setFormData] = useState([]);
+
+  const handleInputChange = (name, value) =>{
+
+    if(name == "noOfDays"){
+      if(value > 8) {
+        value = 8;
+        // alert("Maximum 8 days allowed!");
+      }
+    }
+    setFormData({
+      ...formData,
+      [name]:value
+    })
+  }
+
+  useEffect(() => {
+    console.log(formData)
+  }, [formData])
+
+  const onGenerateTrip = () =>{
+    console.log(formData)
+  }
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center mt-11 ">
+      <div className="flex flex-col items-center justify-center mt-11 mb-11 ">
         <div className="md:w-2/3 w-11/12 flex flex-col items-start justify-start gap-3 text-wrap ">
           <div>
           <h1 className="md:text-3xl text-xl font-bold">Tell us your Travel Preferences 🏕️🌴</h1>
@@ -21,7 +55,9 @@ const CreateTrip = () => {
               className='w-full'
               selectProps={{
                 place,
-                onChange:(v) => {setPlace(v);console.log(v)}
+                onChange:(v) => {setPlace(v);
+                  handleInputChange("location", v)
+                }
               }}
             />
             </div>
@@ -29,13 +65,122 @@ const CreateTrip = () => {
           <div className="w-full flex flex-col items-start justify-start gap-1  ">
             <h2 className="md:text-xl text-lg font-medium my-3">How many days are you planning for?</h2>
             <div className='w-full'>
-              <Input placeholder='Ex. 5' type='number' max={20} min={1} maxLength={2} onInput={(e) => e.target.value = e.target.value.slice(0,2)}/>
+              <div className="flex flex-col gap-1 w-full">
+                <Input 
+                  placeholder='Ex. 5' 
+                  type='number' 
+                  min={1} 
+                  maxLength={2}
+                  onChange={(e) => {
+                    const value = parseInt(e.target.value);
+                    if (value > 8) {
+                      e.target.value = '';
+                      handleInputChange("noOfDays", ''); 
+                      console.log("user has entered and value beyond the range")
+                    } else {
+                      handleInputChange("noOfDays", value)
+                    }
+                  }}
+                />
+                {formData?.noOfDays == "" && (
+                  <p className="text-red-500 text-sm">Please enter a value between 1-8 days</p>
+                )}
+              </div>
             </div>
           </div>
 
           <div className="w-full flex flex-col items-start justify-start gap-1  ">
             <h2 className="md:text-xl text-lg font-medium my-3">What is Your Budget?</h2>
+
+              <div className='w-full grid md:grid-cols-3 grid-cols-2 gap-5'>
+                {
+                SelectBudgetOptions.map((item, index)=>(
+                  <div key={index} onClick={()=> handleInputChange("Budget", item.title)} 
+                  className={
+                    ` cursor-pointer p-3 w-full border flex justify-start items-start flex-col rounded-lg hover:bg-purple-100 transition-all 
+                    ${formData?.Budget==item.title&&`bg-purple-50 border border-purple-500`}`
+                  }>
+                    <div className='w-full flex justify-between items-center'>
+                    <h2 className='text-4xl'>{item.icon}</h2>
+                    <TooltipProvider delayDuration={0}>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <Button 
+                            variant="ghost" 
+                            className='w-8 h-8 bg-purple-200 p-1 rounded-full hover:bg-purple-400'
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            <FaQuestion className='text-purple-900 text-sm'/>
+                          </Button>
+                        </TooltipTrigger>
+                        <TooltipContent 
+                          side="bottom" 
+                          sideOffset={5}
+                          className="max-w-[200px] text-sm break-words"
+                        >
+                          <p>{item.estimatedBudget}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+
+                    </div>
+                    <h2 className='font-bold text-lg mt-1'>{item.title}</h2>
+                    <p className='font-light text-sm text-gray-500'>{item.desc}</p>
+                  </div>
+                ))
+                }
+              </div>
              
+          </div>
+
+          <div className="w-full flex flex-col items-start justify-start gap-1  ">
+            <h2 className="md:text-xl text-lg font-medium my-3">Who do you plan on traveling with on your next adventure?</h2>
+
+              <div className='w-full grid md:grid-cols-3 grid-cols-2 gap-5'>
+                {
+                SelectTravelesList.map((item, index)=>(
+                  <div key={index} onClick={()=> handleInputChange("People", item.people)}
+                  className={
+                    `cursor-pointer p-3 w-full border flex justify-start items-start flex-col rounded-lg hover:bg-purple-100 transition-all 
+                    ${formData?.People==item.people&&`bg-purple-50 border border-purple-500`}`
+                  }>
+                  <div className='w-full flex justify-between items-center'>
+                    <h2 className='text-4xl'>{item.icon}</h2>
+                    <TooltipProvider delayDuration={0}>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                        <Button 
+                            variant="ghost" 
+                            className='w-8 h-8 bg-purple-200 p-1 rounded-full hover:bg-purple-400'
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            <FaQuestion className='text-purple-900 text-sm'/>
+                          </Button>
+                        </TooltipTrigger>
+                        <TooltipContent 
+                          side="bottom" 
+                          sideOffset={5}
+                          className="max-w-[200px] text-sm break-words"
+                        >
+                          <p>{item.toolTipText}</p>
+                          <p>{item.estimatedBudget}</p>
+
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+
+                    </div>
+                    <h2 className='font-bold text-lg mt-3'>{item.title}</h2>
+                    <p className='font-light text-sm text-gray-500'>{item.desc}</p>
+                  </div>
+                ))
+                }
+              </div>
+             
+          </div>
+
+          <div className=' w-full flex justify-end'>
+            <Button onClick={onGenerateTrip} >Generate Trip</Button>
           </div>
 
         </div>
